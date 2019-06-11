449c3b5949ba6d3d57e509137d7403094024c5b2
Narkis Kremizi <narkis.kremizi@live.biu.ac.il>
2019-06-11 18:42:59 +0300

    client connect

--- app/src/main/java/com/example/ex4/JoystickActivity.java
+++ app/src/main/java/com/example/ex4/JoystickActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class JoystickActivity extends AppCompatActivity {
+    private Client client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +14,9 @@ public class JoystickActivity extends AppCompatActivity {
         Intent intent=getIntent();
         String ip= intent.getStringExtra("IP");
         int port=intent.getIntExtra("Port",1234);
+
+        //connect to server
+        client = new Client(ip, port);
+        client.Connect();
     }
 }
